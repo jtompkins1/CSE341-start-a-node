2262edbd861250399343b01cd0b0bf4296ad2ad2
jtompkins1 <jennifermtompkins@gmail.com>
2026-01-19 05:00:53 -0600

    swagger success

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 How to start a new node.js project: https://byui-cse.github.io/cse341-ww-course/week01/project.html
+
+to install swagger:
+npm install swagger-ui-express
+npm install swagger-autogen
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongodb.initDb((err) => {
   if (err) {
